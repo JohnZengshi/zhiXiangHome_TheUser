931896a15e05e241135992a231267d616b942edb
John <1351009316@qq.com>
2018-12-25 20:18:03 +0800

    Merge branch 'feature/feature_v1.0.0_小程序我的页面部分' into develop

---
+++ src/pages/mine/index.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="minePage">
+    <div class="userVia display_flex flex-direction_column align-items_center justify-content_flex-justify">
+      <div class="via display_flex flex-direction_column justify-content_flex-center align-items_center">
+        <div>
+          <open-data type="userAvatarUrl"></open-data>
+        </div>
+      </div>
+      <span class="name">
+        <open-data type="userNickName"></open-data>
+      </span>
+
+    </div>
+    <div class="menuList">
+      <ul>
+        <li @click="concentKF" class="display_flex justify-content_flex-justify align-items_center">
+          <div class="display_flex align-items_center">
+            <img src="/static/images/mainPage/contactCustomerService-icon.png" alt="">
+            <span>联系客服</span>
+          </div>
+          <div class="display_flex align-items_center">
+            <span>400-2365-5698</span>
+            <img src="/static/images/mainPage/arrows.png" alt="">
+          </div>
+        </li>
+        <navigator url="/pages/mine/aboutUs/main">
+          <li class="display_flex justify-content_flex-justify align-items_center">
+            <div class="display_flex align-items_center">
+              <img src="/static/images/mainPage/aboutUs.png" alt="">
+              <span>关于我们</span>
+            </div>
+            <div class="display_flex align-items_center">
+              <img src="/static/images/mainPage/arrows.png" alt="">
+            </div>
+          </li>
+        </navigator>
+        <navigator url="/pages/mine/mineSetting/main">
+          <li class="display_flex justify-content_flex-justify align-items_center">
+            <div class="display_flex align-items_center">
+              <img src="/static/images/mainPage/setting-icon.png" alt="">
+              <span>设置</span>
+            </div>
+            <div class="display_flex align-items_center">
+              <img src="/static/images/mainPage/arrows.png" alt="">
+            </div>
+          </li>
+        </navigator>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+  import {
+    makePhoneCall
+  } from "@/utils/wxapi";
+  export default {
+    computed: {},
+    methods: {
+      concentKF() {
+        makePhoneCall("400-2365-5698")
+      }
+    },
+    onLoad() {}
+  }
+</script>
+<style lang="less" scoped>
+  .minePage {
+    width: 100%;
+    height: 100%;
+    background-color: #F5F5F5;
+
+    .userVia {
+      width: 750rpx;
+      height: 412rpx;
+      background-color: @baseColor;
+
+      .via {
+        width: 262rpx;
+        height: 262rpx;
+        border-radius: 50%;
+        background-color: rgba(78, 62, 51, 0.2);
+        margin-top: 32rpx;
+        overflow: hidden;
+
+        >div {
+          width: 240rpx;
+          height: 240rpx;
+          background-color: #fff;
+          border-radius: 50%;
+          overflow: hidden;
+        }
+      }
+
+      .name {
+        font-size: 34rpx;
+        color: #4D3D33;
+        line-height: 48rpx;
+        margin-bottom: 46rpx;
+        font-family: @fontFamily;
+        font-weight: 600;
+      }
+    }
+
+    .menuList {
+      margin-top: 16rpx;
+
+      >ul {
+        li {
+          width: 100%;
+          height: 94rpx;
+          background-color: #FFFFFF;
+          box-sizing: border-box;
+
+          >div {
+            &:first-of-type {
+              img {
+                width: 54rpx;
+                height: 54rpx;
+                margin-left: 30rpx;
+              }
+
+              >span {
+                font-size: 32rpx;
+                font-family: PingFangSC-Regular;
+                font-weight: 400;
+                color: rgba(77, 61, 51, 1);
+                margin-left: 30rpx;
+              }
+            }
+
+            &:last-of-type {
+              >span {
+                font-size: 32rpx;
+                font-family: PingFangSC-Regular;
+                font-weight: 400;
+                color: rgba(77, 61, 51, 1);
+                margin-right: 17rpx;
+              }
+
+              img {
+                width: 13rpx;
+                height: 23rpx;
+                margin-right: 30rpx;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
---
+++ src/pages/mine/mineSetting/changePhone/index.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="changePhonePage">
+    <ul>
+      <li class="display_flex justify-content_flex-justify align-items_center">
+        <input v-model="phone" type="number" placeholder="请输入手机号" placeholder-class="placeholderClass001">
+        <SendCode @getSendCode="getSendCode" :phoneNum="phone"></SendCode>
+      </li>
+      <li>
+        <input v-model="code" type="text" placeholder="请输入验证码" placeholder-class="placeholderClass001">
+      </li>
+    </ul>
+    <button @click="confirm" class="btn confirmBtn">提交</button>
+  </div>
+</template>
+<script>
+  //   import {
+  //     changePhone,
+  //     sendSmsCode,
+  //     checkSmsCode
+  //   } from "@/network/api";
+  import {
+    navigateTo,
+    navigateBack,
+    toast
+  } from "@/utils/wxapi.js";
+  import {
+    RegExpr
+  } from "@/utils/regex.js"
+  import SendCode from "@/components/sendCode.vue";
+  export default {
+    data() {
+      return {
+        phone: "",
+        code: "",
+      }
+    },
+    components: {
+      SendCode,
+    },
+    computed: {
+      sendSmsCodeParams() { //发送短信验证码参数
+        return {
+          openid: this.globalData.openId,
+          phone: this.phone,
+          type: "change_phone",
+        }
+      },
+      checkSmsCodeParams() { //验证验证码参数
+        return {
+          openid: this.globalData.openId,
+          phone: this.phone,
+          code: this.code,
+          type: "change_phone"
+        }
+      },
+      changePhoneParams() { //更换手机参数
+        return {
+          openid: this.globalData.openId,
+          phone: this.phone,
+          old_phone: this.globalData.userInfo ? this.globalData.userInfo.phone : "",
+        }
+      }
+    },
+    methods: {
+      getSendCode() {; //获取验证码
+        // (async () => {
+        //   let sendSmsCodeRES = await sendSmsCode(this.sendSmsCodeParams)
+        //   console.log(sendSmsCodeRES);
+        //   if (sendSmsCodeRES.errCode == 0) {
+        //     toast(sendSmsCodeRES.msg);
+        //   } else {
+        //     toast(sendSmsCodeRES.errMsg)
+        //   }
+        // })()
+      },
+      confirm() {
+        // let isPhone = RegExpr.checkMobile(this.phone)
+        // if (!isPhone) {
+        //   toast("请输入正确的手机号")
+        // } else if (this.code == '') {
+        //   toast("验证码不能为空")
+        // } else {
+        //   console.log("输入无误");;
+        //   (async () => {
+        //     let checkSmsCodeRES = await checkSmsCode(this.checkSmsCodeParams);
+        //     if (checkSmsCodeRES.errCode == 0) {
+        //       let changePhoneRES = await changePhone(this.changePhoneParams);
+        //       if (changePhoneRES.errCode == 0) {
+        //         await toast(changePhoneRES.msg, 1000);
+        //         navigateBack();
+        //       } else {
+        //         toast(changePhoneRES.msg)
+        //       }
+        //     } else {
+        //       toast(checkSmsCodeRES.msg)
+        //     }
+
+        //   })()
+        // }
+      }
+    }
+  }
+</script>
+<style lang="less" scoped>
+  .changePhonePage {
+    width: 100%;
+    height: 100%;
+    padding: 36rpx 30rpx;
+    box-sizing: border-box;
+
+    >ul {
+      li {
+        margin-top: 26rpx;
+        border-bottom: 1rpx solid #E4E4E4;
+
+        input {
+          height: 94rpx;
+          width: 100%;
+        }
+      }
+    }
+    >.btn{
+      margin-top: 70rpx;
+    }
+  }
+</style>
